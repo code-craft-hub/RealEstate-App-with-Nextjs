438eae616f5b040e07755376a18d626ef6e94c6f
Kachi <onlinehassle1234@gmail.com>
2023-06-05 08:31:30 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
+
+module.exports = {
+    images: {
+      domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+    },
+  };
+  
